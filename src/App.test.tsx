7425f3f4f55475d9688e7e6574e422a509f98a8c
Peter Hyder <peter.c.t.hyder@googlemail.com>
2021-12-16 21:59:04 +0000

    Added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { App } from './App'
 
 test('renders the title', () => {
     render(<App />)
     const linkElement = screen.getByText(/Space Loops/i)
     expect(linkElement).toBeInTheDocument()
+})
+
+test('renders the search button', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/SEARCH/i)
+    expect(linkElement).toBeInTheDocument()
+})
+
+test('can search', async () => {
+    render(<App />)
+    const linkElement = screen.getByText(/SEARCH/i)
+    fireEvent.click(linkElement)
+    await waitFor(() => {
+        const image = screen.getByAltText(
+            'https://images-assets.nasa.gov/image/jsc2007e034221/jsc2007e034221~thumb.jpg'
+        )
+        expect(image).toBeTruthy()
+    })
 })
